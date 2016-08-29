bc841ce443a2828ea2c9849646362ec01218eecf
zhouziqunzzq <zhouziqun@cool2645.com>
2016-08-29 05:41:55 +0000

    Initial.

---
+++ database/migrations/2016_08_25_083503_create_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('stunum');  //学号
+            $table->enum('gender',['male','female']);
+            $table->enum('applicant',['播音','合成','采编','策划']);  //申请
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('students');
+    }
+}
